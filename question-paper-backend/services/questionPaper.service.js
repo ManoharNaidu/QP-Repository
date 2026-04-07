b7456bb0860715419defaae4aa5a71f6102a6d5a
ManoharNaidu <beesettim27@gmail.com>
2026-04-07 10:32:11 +1000

    added prediction route and fixed few bugs

--- question-paper-backend/services/questionPaper.service.js
+++ question-paper-backend/services/questionPaper.service.js
@@ -1,4 +1,4 @@
-import pdf from "pdf-parse";
+import pdf from "pdf-parse/lib/pdf-parse.js";
 import QuestionPaper from "../models/QuestionPaper.js";
 import configureCloudinary from "../config/cloudinary.config.js";
 import {
